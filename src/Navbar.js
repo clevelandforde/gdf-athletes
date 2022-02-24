a6d1f76dc46f059df5e03c990c1a2df2c21252e7
Cleveland Forde <cf.services.gy@gmail.com>
2022-02-23 21:17:58 -0400

    Add styles
    Full React Tutorial #6 - Adding styles

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    
+
+    return ( 
+        <nav className="navbar">
+            <h1>A GDF Athletes Registration Form</h1>
+            <div className="links">
+                <a href="/">Home</ a> 
+                <a href="/create">New Form</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
